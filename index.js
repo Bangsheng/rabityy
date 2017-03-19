bfb0e02707bd84cc11bb2bb999c582482f7b3552
Bangsheng Sui <bsui.strade@gmail.com>
2017-03-19 13:18:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+
+render((
+  <Router>
+  </Router>
+), document.getElementById('app'))
