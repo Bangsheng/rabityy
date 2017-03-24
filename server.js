9270faa0aa2ba92b1736ee7b5d7f4184d95521b0
Bangsheng Sui <suibangsheng@gmail.com>
2017-03-24 13:52:50 -0500

    script change

---
+++ server.js
@@ -0,0 +1,15 @@
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+import webpackConfig from './webpack.config';
+
+const compiler = webpack(webpackConfig);
+const port = process.env.PORT || 8000;
+const server = new WebpackDevServer(compiler, {
+  contentBase: '.',
+  port: process.env.PORT || 8000,
+  compress: true,
+  historyApiFallback: true,
+  publicPath: '/public/',
+});
+
+server.listen(port, err => console.log(err));
